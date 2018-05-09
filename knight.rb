38069bea1d0034960f57ced59dd89a628a003c4d
Punnadit Trussat <punnadittr@gmail.com>
2018-05-09 10:06:27 +0700

    finished knight traversal program

--- knight.rb
+++ knight.rb
@@ -1,25 +1,25 @@
 class Node
-  attr_accessor :children, :position #value
+  attr_accessor :children, :position, :parent
 
-  def initialize(position = [3,4])
+  def initialize(position = [3,4], parent = nil)
     @position = position
+    @parent = parent
     @children = []
   end
 
   def add_child(child_node)
     @children << child_node
   end
-
 end
 
 class Tree
   attr_reader :possible_moves
 
-  def initialize(position)
-    @initial_move = Node.new(position)
+  def initialize
+   create_all_moves
   end
 
-  def create_possible_moves
+  def create_all_moves
     possible_moves = []
     x = (0..7).to_a
     y = (0..7).to_a
@@ -31,71 +31,66 @@ class Tree
     @possible_moves = possible_moves
   end
 
-  #calculate one possible move at a time
-  def cal(x, y, a, b, node)
-    output_1 = []
-    output_2 = []
-    output_1 << x + a << y + b
-    output_2 << x + b << y + a
-    if (output_1.all? { |pos| pos >= 0 && pos <= 7 }) && (@possible_moves.include? output_1)
-      node.add_child(Node.new(output_1))
-      @possible_moves.delete(output_1)
-    end
-    if (output_2.all? { |pos| pos >= 0 && pos <= 7 }) && (@possible_moves.include? output_2)
-      node.add_child(Node.new(output_2))
-      @possible_moves.delete(output_2)
+  # get one possible move of one position
+  def cal(a, b, node)
+    x = node.position[0]
+    y = node.position[1]
+    output = []
+    output.push([x+a, y+b],[x+b, y+a])
+    output.each do |pos|
+      if @possible_moves.include? pos
+        node.add_child(Node.new(pos, node))
+        @possible_moves.delete(pos)
+      end
     end
   end
 
   #get all possible moves of one position
   def calc_move(node)
     @possible_moves.delete(node.position)
-    x = node.position[0]
-    y = node.position[1]
-    cal(x, y, -1, -2, node)
-    cal(x, y, -1, 2, node)
-    cal(x, y, +1, -2, node)
-    cal(x, y, 1, 2, node)
+    cal(-1, -2, node)
+    cal(-1, 2, node)
+    cal(+1, -2, node)
+    cal(1, 2, node)
   end
-
-  def traverse
-    @stack = []
-    @stack << @initial_move
-    until @stack.empty?
-      current_node = @stack.shift
+  # get all possible moves of all positions
+  def find_all_moves
+    stack = []
+    stack << @initial_move
+    until stack.empty?
+      current_node = stack.shift
       calc_move(current_node)
-      @stack << current_node.children
-      @stack.flatten!
-    end
-  end
-
-  def to_s
-    stack = []
-    stack << @initial_move
-    until stack.empty?
-      current_node = stack.shift
-      print current_node.position
       stack << current_node.children
       stack.flatten!
     end
   end
 
-  def shortest(path)
+  def knight_moves(first_move, second_move)
+    @initial_move = Node.new(first_move)
+    create_all_moves
+    find_all_moves
     stack = []
+    output = []
     stack << @initial_move
     until stack.empty?
       current_node = stack.shift
-      return current_node if current_node.position == path
-      stack << current_node.children
-      stack.flatten!
+      if current_node.position == second_move
+        until current_node.nil?
+          output << current_node.position
+          current_node = current_node.parent
+        end
+        puts "You made it in #{output.size-1} moves! Here's your path:"
+        return output.reverse.each { |pos| print pos; puts }
+      else
+        stack << current_node.children
+        stack.flatten!
+      end
     end
   end
-
-  def stack
-    @stack
-  end
-
 end
-mytree = Tree.new([3,4])
-mytree.create_possible_moves
-mytree.traverse
+
+mytree = Tree.new
+#test
+1000.times do
+  mytree.knight_moves([rand(0..7),rand(0..7)], [rand(0..7),rand(0..7)])
+end
